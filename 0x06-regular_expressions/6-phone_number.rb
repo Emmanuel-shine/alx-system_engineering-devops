16c23e0284839bb5254c39ab5796ab4bc2cf0a0e
Emmanuel-shine <emmanuelshine9@gmail.com>
2024-05-18 18:28:29 -0700

    regular

---
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+# This script accepts one argument and matches a specific regular expression pattern for a 10-digit phone number.
+# The first argumment from the command line
+input = ARGV[0]
+The regular expression for matching a 10-digit phone number
+regex = /^\d{10}$/
+# Find all matches and join them to output in one line
+matches = input.scan(regex).join
+# Print the result
+puts matches
+
