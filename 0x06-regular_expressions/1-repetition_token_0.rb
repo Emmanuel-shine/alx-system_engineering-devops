047889bb431e3cda73172bc63e4da84022d98c22
Emmanuel-shine <emmanuelshine9@gmail.com>
2024-05-18 17:39:30 -0700

    regular

---
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+# This script accepts one argument and matches a specific regular expression pattern.
+# The first argument from the command line
+input = ARGV[0]
+# The regular expression for the given task, it matches patterns like htn, hbtn, hbttn, hbt+tn
+regex = /hbt{2,5}n/
+# Find all matches and join them to output in one line
+matches = input.scan(regex).join
+# Print the result
+puts matches
+
