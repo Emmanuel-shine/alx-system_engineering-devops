dc891f590dd34bc3e46af6c9cd36ec3cf40e2081
Emmanuel-shine <emmanuelshine9@gmail.com>
2024-05-18 19:10:01 -0700

    regular

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -3,7 +3,7 @@
 # The first argumment from the command line
 input = ARGV[0]
 #The regular expression for matching a 10-digit phone number
-regex = /^\d{10}$/
+regex = /[A-Z]+/
 # Find all matches and join them to output in one line
 matches = input.scan(regex).join
 # Print the result
