2001dd438ec75492d2557265a4999ea4eaf15ea9
Emmanuel-shine <emmanuelshine9@gmail.com>
2024-08-13 14:23:28 +0000

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,20 +6,20 @@ This module contains the number_of_subscribers function.
 import requests
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers
-    for a given subreddit.
+	"""
+	Queries the Reddit API and returns the number of subscribers
+	for a given subreddit.
 
-    If the subreddit is invalid, returns 0.
+	If the subreddit is invalid, returns 0.
 
-    :param subreddit: The name of the subreddit to query.
-    :return: The number of subscribers or 0 if invalid.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'custom-agent'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+	:param subreddit: The name of the subreddit to query.
+	:return: The number of subscribers or 0 if invalid.
+	"""
+	url = f"https://www.reddit.com/r/{subreddit}/about.json"
+	headers = {'User-Agent': 'custom-agent'}
+	response = requests.get(url, headers=headers, allow_redirects=False)
     
-    if response.status_code == 200:
-        data = response.json().get("data", {})
-        return data.get("subscribers", 0)
-    return 0
+	if response.status_code == 200:
+        	data = response.json().get("data", {})
+        	return data.get("subscribers", 0)
+		return 0
